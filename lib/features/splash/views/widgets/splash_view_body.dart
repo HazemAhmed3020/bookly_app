d3c8668e886729a859b7e3e4664bf908c6cd3717
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-18 00:55:29 +0200

    extract methods

--- lib/features/splash/views/widgets/splash_view_body.dart
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -18,15 +18,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 10),
-      end: Offset.zero,
-    ).animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
 
   }
   @override
@@ -59,5 +51,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
       ],
     );
+  }
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
   }
 }
