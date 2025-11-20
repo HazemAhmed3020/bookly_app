1882c01411963d7c1e7399ccd9d90075f5839cb7
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-20 02:18:38 +0200

    add image and title and subtitle to book details

--- lib/features/home_page/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home_page/presentation/view/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home_page/presentation/view/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'custom_book_details_appbar.dart';
 
@@ -7,9 +8,32 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+  var width =  MediaQuery.of(context).size.width;
+    return Column(
           children: [
-              CustomBookDetailsAppbar(),
+              const CustomBookDetailsAppbar(),
+              const SizedBox(height: 5,),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: width * .24,
+                ),
+                  child: const CustomBookItem()
+              ),
+            const SizedBox(height: 40,),
+            const Text('The Jungle Book', style: TextStyle(fontSize: 30 , color: Colors.white , fontFamily: 'GT Sectra Fine Regular'),),
+            const SizedBox(height: 10,),
+            const Text('J.K. Rowling', style: TextStyle(color: Colors.blueGrey , fontWeight: FontWeight.w500 , fontSize: 20), textAlign: TextAlign.left,),
+            const SizedBox(height: 20,),
+            SizedBox(
+              height: 18,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('⭐ 4.8 ', textAlign: TextAlign.end,style: TextStyle(color: Colors.white , fontSize: 15 , fontWeight: FontWeight.w500) ),
+                  const Text(' (2390)', textAlign: TextAlign.end,style: TextStyle(color: Colors.white12 , fontSize: 15 , fontWeight: FontWeight.w500) ),
+                ],
+              ),
+            )
           ],
         );
   }
