f321bc5938ac1f5152a4dca5253af86b6224b5d6
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-24 03:25:21 +0200

    handle book model

--- lib/features/home_page/data/models/books_model.dart
+++ lib/features/home_page/data/models/books_model.dart
@@ -1,352 +1,104 @@
-import 'package:equatable/equatable.dart';
-
-class BooksModel extends Equatable {
-  const BooksModel({
-    required this.kind,
-    required this.totalItems,
-    required this.items,
-  });
-
-  final String? kind;
-  final int? totalItems;
-  final List<Item> items;
-
-  factory BooksModel.fromJson(Map<String, dynamic> json){
-    return BooksModel(
-      kind: json["kind"],
-      totalItems: json["totalItems"],
-      items: json["items"] == null ? [] : List<Item>.from(json["items"]!.map((x) => Item.fromJson(x))),
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    kind, totalItems, items, ];
-}
-
-class Item extends Equatable {
-  const Item({
-    required this.kind,
-    required this.id,
-    required this.etag,
-    required this.selfLink,
-    required this.volumeInfo,
-    required this.saleInfo,
-    required this.accessInfo,
-    required this.searchInfo,
-  });
-
-  final String? kind;
+class BookModel {
   final String? id;
-  final String? etag;
-  final String? selfLink;
   final VolumeInfo? volumeInfo;
   final SaleInfo? saleInfo;
-  final AccessInfo? accessInfo;
-  final SearchInfo? searchInfo;
 
-  factory Item.fromJson(Map<String, dynamic> json){
-    return Item(
-      kind: json["kind"],
-      id: json["id"],
-      etag: json["etag"],
-      selfLink: json["selfLink"],
-      volumeInfo: json["volumeInfo"] == null ? null : VolumeInfo.fromJson(json["volumeInfo"]),
-      saleInfo: json["saleInfo"] == null ? null : SaleInfo.fromJson(json["saleInfo"]),
-      accessInfo: json["accessInfo"] == null ? null : AccessInfo.fromJson(json["accessInfo"]),
-      searchInfo: json["searchInfo"] == null ? null : SearchInfo.fromJson(json["searchInfo"]),
+  BookModel({this.id, this.volumeInfo, this.saleInfo});
+
+  factory BookModel.fromJson(Map<String, dynamic> json) {
+    return BookModel(
+      id: json['id'],
+      volumeInfo: json['volumeInfo'] != null
+          ? VolumeInfo.fromJson(json['volumeInfo'])
+          : null,
+      saleInfo: json['saleInfo'] != null
+          ? SaleInfo.fromJson(json['saleInfo'])
+          : null,
     );
   }
 
-  @override
-  List<Object?> get props => [
-    kind, id, etag, selfLink, volumeInfo, saleInfo, accessInfo, searchInfo, ];
+
+  String get image => volumeInfo?.imageLinks?.thumbnail ?? 'https://via.placeholder.com/150';
+
+  String get title => volumeInfo?.title ?? 'No Title';
+
+  String get author => (volumeInfo?.authors?.isNotEmpty ?? false)
+      ? volumeInfo!.authors![0]
+      : 'Unknown Author';
+
+  String get price => saleInfo?.listPrice?.amount?.toString() ?? 'Free';
+
+  num get rating => volumeInfo?.averageRating ?? 0;
+
+  num get count => volumeInfo?.ratingsCount ?? 0;
 }
 
-class AccessInfo extends Equatable {
-  const AccessInfo({
-    required this.country,
-    required this.viewability,
-    required this.embeddable,
-    required this.publicDomain,
-    required this.textToSpeechPermission,
-    required this.epub,
-    required this.pdf,
-    required this.webReaderLink,
-    required this.accessViewStatus,
-    required this.quoteSharingAllowed,
-  });
-
-  final String? country;
-  final String? viewability;
-  final bool? embeddable;
-  final bool? publicDomain;
-  final String? textToSpeechPermission;
-  final Epub? epub;
-  final Pdf? pdf;
-  final String? webReaderLink;
-  final String? accessViewStatus;
-  final bool? quoteSharingAllowed;
-
-  factory AccessInfo.fromJson(Map<String, dynamic> json){
-    return AccessInfo(
-      country: json["country"],
-      viewability: json["viewability"],
-      embeddable: json["embeddable"],
-      publicDomain: json["publicDomain"],
-      textToSpeechPermission: json["textToSpeechPermission"],
-      epub: json["epub"] == null ? null : Epub.fromJson(json["epub"]),
-      pdf: json["pdf"] == null ? null : Pdf.fromJson(json["pdf"]),
-      webReaderLink: json["webReaderLink"],
-      accessViewStatus: json["accessViewStatus"],
-      quoteSharingAllowed: json["quoteSharingAllowed"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    country, viewability, embeddable, publicDomain, textToSpeechPermission, epub, pdf, webReaderLink, accessViewStatus, quoteSharingAllowed, ];
-}
-
-class Epub extends Equatable {
-  const Epub({
-    required this.isAvailable,
-    required this.downloadLink,
-  });
-
-  final bool? isAvailable;
-  final String? downloadLink;
-
-  factory Epub.fromJson(Map<String, dynamic> json){
-    return Epub(
-      isAvailable: json["isAvailable"],
-      downloadLink: json["downloadLink"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    isAvailable, downloadLink, ];
-}
-
-class Pdf extends Equatable {
-  const Pdf({
-    required this.isAvailable,
-  });
-
-  final bool? isAvailable;
-
-  factory Pdf.fromJson(Map<String, dynamic> json){
-    return Pdf(
-      isAvailable: json["isAvailable"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    isAvailable, ];
-}
-
-class SaleInfo extends Equatable {
-  const SaleInfo({
-    required this.country,
-    required this.saleability,
-    required this.isEbook,
-    required this.buyLink,
-  });
-
-  final String? country;
-  final String? saleability;
-  final bool? isEbook;
-  final String? buyLink;
-
-  factory SaleInfo.fromJson(Map<String, dynamic> json){
-    return SaleInfo(
-      country: json["country"],
-      saleability: json["saleability"],
-      isEbook: json["isEbook"],
-      buyLink: json["buyLink"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    country, saleability, isEbook, buyLink, ];
-}
-
-class SearchInfo extends Equatable {
-  const SearchInfo({
-    required this.textSnippet,
-  });
-
-  final String? textSnippet;
-
-  factory SearchInfo.fromJson(Map<String, dynamic> json){
-    return SearchInfo(
-      textSnippet: json["textSnippet"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    textSnippet, ];
-}
-
-class VolumeInfo extends Equatable {
-  const VolumeInfo({
-    required this.title,
-    required this.authors,
-    required this.publishedDate,
-    required this.industryIdentifiers,
-    required this.readingModes,
-    required this.pageCount,
-    required this.printType,
-    required this.categories,
-    required this.maturityRating,
-    required this.allowAnonLogging,
-    required this.contentVersion,
-    required this.panelizationSummary,
-    required this.imageLinks,
-    required this.language,
-    required this.previewLink,
-    required this.infoLink,
-    required this.canonicalVolumeLink,
-    required this.publisher,
-    required this.description,
-    required this.subtitle,
-  });
-
+class VolumeInfo {
   final String? title;
-  final List<String> authors;
-  final String? publishedDate;
-  final List<IndustryIdentifier> industryIdentifiers;
-  final ReadingModes? readingModes;
-  final int? pageCount;
-  final String? printType;
-  final List<String> categories;
-  final String? maturityRating;
-  final bool? allowAnonLogging;
-  final String? contentVersion;
-  final PanelizationSummary? panelizationSummary;
+  final List<String>? authors;
   final ImageLinks? imageLinks;
-  final String? language;
-  final String? previewLink;
-  final String? infoLink;
-  final String? canonicalVolumeLink;
-  final String? publisher;
-  final String? description;
-  final String? subtitle;
+  final num? averageRating;
+  final num? ratingsCount;
 
-  factory VolumeInfo.fromJson(Map<String, dynamic> json){
-    return VolumeInfo(
-      title: json["title"],
-      authors: json["authors"] == null ? [] : List<String>.from(json["authors"]!.map((x) => x)),
-      publishedDate: json["publishedDate"],
-      industryIdentifiers: json["industryIdentifiers"] == null ? [] : List<IndustryIdentifier>.from(json["industryIdentifiers"]!.map((x) => IndustryIdentifier.fromJson(x))),
-      readingModes: json["readingModes"] == null ? null : ReadingModes.fromJson(json["readingModes"]),
-      pageCount: json["pageCount"],
-      printType: json["printType"],
-      categories: json["categories"] == null ? [] : List<String>.from(json["categories"]!.map((x) => x)),
-      maturityRating: json["maturityRating"],
-      allowAnonLogging: json["allowAnonLogging"],
-      contentVersion: json["contentVersion"],
-      panelizationSummary: json["panelizationSummary"] == null ? null : PanelizationSummary.fromJson(json["panelizationSummary"]),
-      imageLinks: json["imageLinks"] == null ? null : ImageLinks.fromJson(json["imageLinks"]),
-      language: json["language"],
-      previewLink: json["previewLink"],
-      infoLink: json["infoLink"],
-      canonicalVolumeLink: json["canonicalVolumeLink"],
-      publisher: json["publisher"],
-      description: json["description"],
-      subtitle: json["subtitle"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    title, authors, publishedDate, industryIdentifiers, readingModes, pageCount, printType, categories, maturityRating, allowAnonLogging, contentVersion, panelizationSummary, imageLinks, language, previewLink, infoLink, canonicalVolumeLink, publisher, description, subtitle, ];
-}
-
-class ImageLinks extends Equatable {
-  const ImageLinks({
-    required this.smallThumbnail,
-    required this.thumbnail,
+  VolumeInfo({
+    this.title,
+    this.authors,
+    this.imageLinks,
+    this.averageRating,
+    this.ratingsCount,
   });
 
+  factory VolumeInfo.fromJson(Map<String, dynamic> json) {
+    return VolumeInfo(
+      title: json['title'],
+      authors: (json['authors'] as List<dynamic>?)?.map((e) => e.toString()).toList(),
+      imageLinks: json['imageLinks'] != null
+          ? ImageLinks.fromJson(json['imageLinks'])
+          : null,
+      averageRating: json['averageRating'],
+      ratingsCount: json['ratingsCount'],
+    );
+  }
+}
+
+class ImageLinks {
   final String? smallThumbnail;
   final String? thumbnail;
 
-  factory ImageLinks.fromJson(Map<String, dynamic> json){
+  ImageLinks({this.smallThumbnail, this.thumbnail});
+
+  factory ImageLinks.fromJson(Map<String, dynamic> json) {
     return ImageLinks(
-      smallThumbnail: json["smallThumbnail"],
-      thumbnail: json["thumbnail"],
+      smallThumbnail: json['smallThumbnail'],
+      thumbnail: json['thumbnail'],
     );
   }
-
-  @override
-  List<Object?> get props => [
-    smallThumbnail, thumbnail, ];
 }
 
-class IndustryIdentifier extends Equatable {
-  const IndustryIdentifier({
-    required this.type,
-    required this.identifier,
-  });
+class SaleInfo {
+  final ListPrice? listPrice;
 
-  final String? type;
-  final String? identifier;
+  SaleInfo({this.listPrice});
 
-  factory IndustryIdentifier.fromJson(Map<String, dynamic> json){
-    return IndustryIdentifier(
-      type: json["type"],
-      identifier: json["identifier"],
+  factory SaleInfo.fromJson(Map<String, dynamic> json) {
+    return SaleInfo(
+      listPrice: json['listPrice'] != null
+          ? ListPrice.fromJson(json['listPrice'])
+          : null,
     );
   }
-
-  @override
-  List<Object?> get props => [
-    type, identifier, ];
 }
 
-class PanelizationSummary extends Equatable {
-  const PanelizationSummary({
-    required this.containsEpubBubbles,
-    required this.containsImageBubbles,
-  });
+class ListPrice {
+  final num? amount;
+  final String? currencyCode;
 
-  final bool? containsEpubBubbles;
-  final bool? containsImageBubbles;
+  ListPrice({this.amount, this.currencyCode});
 
-  factory PanelizationSummary.fromJson(Map<String, dynamic> json){
-    return PanelizationSummary(
-      containsEpubBubbles: json["containsEpubBubbles"],
-      containsImageBubbles: json["containsImageBubbles"],
+  factory ListPrice.fromJson(Map<String, dynamic> json) {
+    return ListPrice(
+      amount: json['amount'],
+      currencyCode: json['currencyCode'],
     );
   }
-
-  @override
-  List<Object?> get props => [
-    containsEpubBubbles, containsImageBubbles, ];
-}
-
-class ReadingModes extends Equatable {
-  const ReadingModes({
-    required this.text,
-    required this.image,
-  });
-
-  final bool? text;
-  final bool? image;
-
-  factory ReadingModes.fromJson(Map<String, dynamic> json){
-    return ReadingModes(
-      text: json["text"],
-      image: json["image"],
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-    text, image, ];
 }
