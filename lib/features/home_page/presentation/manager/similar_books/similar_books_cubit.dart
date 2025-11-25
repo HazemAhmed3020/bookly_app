2338cee5d9e429344710ddac1b547484c128d157
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-25 02:13:21 +0200

    create similar books cubit

---
+++ lib/features/home_page/presentation/manager/similar_books/similar_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../data/models/books_model.dart';
+import '../../../data/repos/home_repo.dart';
+part 'similar_books_state.dart';
+
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> similarBooks() async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+          (failure) {
+        emit(SimilarBooksFailure(errMessage: failure.errMessage));
+      },
+          (books) {
+        emit(SimilarBooksSuccess(books: books));
+      },
+    );
+  }
+}
