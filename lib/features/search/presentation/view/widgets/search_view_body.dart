6fce680b519b1db50dce667b64b3e13cfaea15be
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-12-27 16:28:56 +0200

    add textfield to search view

--- lib/features/search/presentation/view/widgets/search_view_body.dart
+++ lib/features/search/presentation/view/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -8,32 +8,40 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-            CustomTextField(),
+        const CustomTextField(),
+        const SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Text(
+            'Search result',
+            textAlign: TextAlign.start,
+            style: Styles.meduimdetails,
+          ),
+        ),
+        const SizedBox(height: 20),
       ],
     );
   }
 }
 
- class CustomTextField extends StatelessWidget {
-   const CustomTextField({super.key});
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-     return Padding(
-       padding: const EdgeInsets.symmetric(horizontal: 16),
-       child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Search',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(
-                color: Colors.white60,
-                width: 2,
-              ),
-            ),
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(color: Colors.white60, width: 2),
           ),
-       ),
-     );
-   }
- }
+        ),
+      ),
+    );
+  }
+}
