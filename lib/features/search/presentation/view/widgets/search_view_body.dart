f70315def7de89cb82c6205c09f1763836fe4f2b
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-12-27 15:28:41 +0200

    handle search view

--- lib/features/search/presentation/view/widgets/search_view_body.dart
+++ lib/features/search/presentation/view/widgets/search_view_body.dart
@@ -7,6 +7,33 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+            CustomTextField(),
+      ],
+    );
   }
 }
+
+ class CustomTextField extends StatelessWidget {
+   const CustomTextField({super.key});
+
+   @override
+   Widget build(BuildContext context) {
+     return Padding(
+       padding: const EdgeInsets.symmetric(horizontal: 16),
+       child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                color: Colors.white60,
+                width: 2,
+              ),
+            ),
+          ),
+       ),
+     );
+   }
+ }
