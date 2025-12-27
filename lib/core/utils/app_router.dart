f9b9195eaa32507f96c72d7c3f5516eab76d7a93
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-12-27 15:28:27 +0200

    add search view to approuter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,13 @@
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home_page/data/models/books_model.dart';
 import 'package:bookly_app/features/home_page/presentation/manager/similar_books/similar_books_cubit.dart';
-import 'package:bookly_app/features/home_page/presentation/view/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/home_page/data/repos/home_repo_imple.dart' show HomeRepoImple;
 import '../../features/home_page/presentation/view/book_details_view.dart';
 import '../../features/home_page/presentation/view/home_view.dart';
+import '../../features/search/presentation/view/search_view.dart';
 import '../../features/splash/views/splash_view.dart';
 
 abstract class AppRouter{
